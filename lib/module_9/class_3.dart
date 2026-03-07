69ca3f79e8029113b286dd2430226bf920a5eaef
Mahedi454 <mahedishihab58@gmail.com>
2026-03-08 02:13:02 +0600

    Flutter Button Practice and Visualization

--- lib/module_9/class_3.dart
+++ lib/module_9/class_3.dart
@@ -31,7 +31,7 @@ class class3 extends StatelessWidget {
                     )
           ),
                   onPressed: () {},
-                  child: Text("Button 1")),
+                  child: Text("Button 1")), // ElevatedButton is a button that has a background color and a shadow. It is used for raised buttons.
               SizedBox(height: 20,),
               SizedBox(
                 // width: double.infinity, // full width for mobile and other small screens
@@ -50,9 +50,53 @@ class class3 extends StatelessWidget {
                           // )
                         )
                     ),
-                    onPressed: () {},
+                    onPressed: () {
+                      print("Hello Mahedi");
+                    },
                     child: Text("Button 2")),
               ),
+              SizedBox(height: 20,),
+              OutlinedButton(
+                  style: OutlinedButton.styleFrom(
+
+                  ),
+                  onPressed: () {},
+                  child: Text("Submit")), // OutlinedButton is a button that has a border and no background color. It is used for outlined buttons.
+              SizedBox(height: 20,),
+              TextButton(onPressed: () {}, child: Text("Read More")), // TextButton is a button that has no background color and no border. It is used for text buttons.
+
+              SizedBox(height: 20,), // SizedBox is used to add space between the widgets. Here, it is adding a vertical space of 20 pixels between the widgets.
+
+              Icon(Icons.delete, color: Colors.red, size: 50,), //There are many icons available in flutter, you can use any of them by changing the name of the icon
+
+              IconButton(onPressed: () {
+                print("Call button pressed"); // After pressing the button, this message will be printed in the console
+              }, icon: Icon(Icons.call)),
+
+              GestureDetector( // GestureDetector is a widget that detects gestures. It is used to detect taps, long presses, double taps, etc. on the child widget.
+                  onTap: (){
+                    print("On Tap is working"); // After tapping the text, this message will be printed in the console
+                  },
+                  onLongPress: (){
+                    print("On Long Press is working"); // After long pressing the text, this message will be printed in the console
+                  },
+                  onDoubleTap: (){
+                    print("On Double Tap is working"); // After double tapping the text, this message will be printed in the console
+                  },
+                  child: Text("This is a text")),
+
+              InkWell(
+                onTap: (){
+                  print("InkWell is working"); // After tapping the text, this message will be printed in the console
+                },
+                 onDoubleTap: (){
+                   print("InkWell on Double Tap is working"); // After double tapping the text, this message will be printed in the console
+                 },
+                 onLongPress: (){
+                   print("InkWell on Long Press is working"); // After long pressing the text, this message will be printed in the console
+                 },
+                child: Text("This is a text 2")
+              )
 
             ],
           )
