40318b5f6d3cf2a323fd2535f86fa24127efaa72
Mahedi454 <mahedishihab58@gmail.com>
2026-03-09 01:16:06 +0600

    Padding

--- lib/module_10/class_1.dart
+++ lib/module_10/class_1.dart
@@ -7,28 +7,34 @@ class Module_11_class_1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  helperText: 'Phone Number',// helperText is a text that is displayed below the text field.
-                  hintText: 'Enter your phone number', // hintText is a text that is displayed inside the text field when it is empty.
-                  labelText: 'Phone', // labelText is a text that is displayed above the text field when it is focused.
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                SizedBox(height: 10,),
+                TextField(
+                  decoration: InputDecoration(
+                    helperText: 'Phone Number',// helperText is a text that is displayed below the text field.
+                    hintText: 'Enter your phone number', // hintText is a text that is displayed inside the text field when it is empty.
+                    labelText: 'Phone', // labelText is a text that is displayed above the text field when it is focused.
 
-                  helperStyle: TextStyle(
-                    fontSize: 20,
-                    color: Colors.green,
-                  ),
-                  hintStyle: TextStyle(
-                    fontSize: 20,
-                    color: Colors.red,
-                  ),
+                    helperStyle: TextStyle(
+                      fontSize: 20,
+                      color: Colors.green,
+                    ),
+                    hintStyle: TextStyle(
+                      fontSize: 20,
+                      color: Colors.red,
+                    ),
 
-                  prefixIcon: Icon(Icons.phone),
-                  suffixIcon: Icon(Icons.check)
-                ),
-              )
-            ],
+                    prefixIcon: Icon(Icons.phone),
+                    suffixIcon: Icon(Icons.check),
+
+                    border: OutlineInputBorder()
+                  ),
+                )
+              ],
+            ),
           )
       ),
     );
