3d728bbddd2fceb3d4fe3036899ac45c249bc266
Mahedi454 <mahedishihab58@gmail.com>
2026-03-10 20:25:39 +0600

    Text form with key

--- lib/module_10/class_2.dart
+++ lib/module_10/class_2.dart
@@ -5,6 +5,9 @@ class Module_10_class_2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _formkey = GlobalKey<FormFieldState>();
+    TextEditingController email = TextEditingController();
+    TextEditingController password = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.purple,
@@ -15,24 +18,70 @@ class Module_10_class_2 extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/1280px-Logo_of_YouTube_%282015-2017%29.svg.png",
-           height: 100,
-           width: 200
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            children: [
+              Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/1280px-Logo_of_YouTube_%282015-2017%29.svg.png",
+               height: 100,
+               width: 200
+              ),
+
+              InkWell(
+                onTap: () {
+                  print("YouTube");
+                },
+                child: Image.asset("assets/YT.png",
+                height: 100,
+                width: 200),
+              ),
+
+              SizedBox(
+                height: 40,
+              ),
+
+              Text("Login Here...!",
+              style: TextStyle(fontSize: 25,
+                color: Colors.purple[700],
+              ),
+              ),
+
+              TextFormField(
+                controller: email,
+                decoration: InputDecoration(
+                  hintText: "Enter your email",
+                  labelText: "Email",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: password,
+                decoration: InputDecoration(
+                  hintText: "Enter your password",
+                  labelText: "Password",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: 300,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.purple,
+                    foregroundColor: Colors.white
+                  ),
+                    onPressed: () {},
+                    child: Text("Login")),
+              )],
           ),
-
-          InkWell(
-            onTap: () {
-              print("YouTube");
-            },
-            child: Image.asset("assets/YT.png",
-            height: 100,
-            width: 200),
-          ),
-
-
-        ],
+        ),
       ),
     );
   }
