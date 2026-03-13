1e14a97401b6396de62448137ac5b75f24f3f86b
Mahedi454 <mahedishihab58@gmail.com>
2026-03-13 21:56:09 +0600

    portrait & landscape orientation(responsive)

--- lib/module_10/class_3.dart
+++ lib/module_10/class_3.dart
@@ -20,29 +20,55 @@ class M_10_Class_3 extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Text(orientation.toString()),
-            // Text("Screen width: $screenWidth"),
-            // Text("Screen height: $screenHeight"),
-            Container(
-              height: 150,
-              width: 300,
-              color: Colors.red,
-            ),
-            Text("Mahedi Hasan",
-            style: TextStyle(
-              fontSize: 25,
-            ),
-            ),
-            Container(
-              height: screenHeight * 0.2,
-              width: screenWidth * 0.6,
-              color: Colors.green,
-            ),
-            Text("Shihab",
-              style: TextStyle(
-                fontSize: 25,
+
+          orientation == Orientation.portrait ? Column(
+              children: [
+                Container(
+                  height: 150,
+                  width: 300,
+                  color: Colors.red,
+                ),
+                Text("Mahedi Hasan",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+                Container(
+                  height: screenHeight * 0.2,
+                  width: screenWidth * 0.6,
+                  color: Colors.green,
+                ),
+                Text("Shihab",
+                  style: TextStyle(
+                    fontSize: 25,
+                  ),
+                ),
+              ],
+            ) : Row(
+            children: [
+              Container(
+                height: 150,
+                width: 300,
+                color: Colors.red,
               ),
-            ),
+              Text("Mahedi Hasan",
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+              Container(
+                height: screenHeight * 0.2,
+                width: screenWidth * 0.6,
+                color: Colors.green,
+              ),
+              Text("Shihab",
+                style: TextStyle(
+                  fontSize: 25,
+                ),
+              ),
+            ],
+          ),
+
           ],
         ),
       )
