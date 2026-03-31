4ac3843d2839e5c100d6ff58ec26f75832db6873
Mahedi454 <mahedihasan.codes@gmail.com>
2026-04-01 02:36:46 +0600

    GridView.builder

--- lib/module_11/GridV.dart
+++ lib/module_11/GridV.dart
@@ -9,31 +9,55 @@ class Gridv extends StatelessWidget {
       appBar: AppBar(
         title: Text("Grid"),
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        crossAxisSpacing: 10,
-        mainAxisSpacing: 10,
-        children: [
-          Container(
-            color: Colors.red,
-          )
-          ,Container(
-            color: Colors.green,
-          )
-          ,Container(
-            color: Colors.blue,
+      body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
           ),
-          Container(
-            color: Colors.yellow,
-          ),
-          Container(
-              color: Colors.purple,
-            ),
-          Container(
-              color: Colors.orange,
-            ),
-        ],
+          itemCount: 20,
+          itemBuilder: (context, index){
+            return Card(
+              color: Colors.lightBlueAccent,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Icon(Icons.phone),
+                  Text('Mahedi', style: TextStyle(fontSize: 20),),
+                ]
+              ),
+            );
+          },
       ),
+
+
+
+      // GridView.count(
+      //   crossAxisCount: 2,
+      //   crossAxisSpacing: 10,
+      //   mainAxisSpacing: 10,
+      //   children: [
+      //     Container(
+      //       color: Colors.red,
+      //     )
+      //     ,Container(
+      //       color: Colors.green,
+      //     )
+      //     ,Container(
+      //       color: Colors.blue,
+      //     ),
+      //     Container(
+      //       color: Colors.yellow,
+      //     ),
+      //     Container(
+      //         color: Colors.purple,
+      //       ),
+      //     Container(
+      //         color: Colors.orange,
+      //       ),
+      //   ],
+      // ),
     );
   }
 }
