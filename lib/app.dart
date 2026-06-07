8f4e367a7d5037e51f803365da317ee50e916091
Mahedi454 <mahedihasan.codes@gmail.com>
2026-06-07 21:01:44 +0600

    TabBar

--- lib/app.dart
+++ lib/app.dart
@@ -15,6 +15,7 @@ import 'Module_12/navi/page3.dart';
 import 'Module_13/class_1.dart';
 import 'Module_13/class_2.dart';
 import 'Module_13/class_3.dart';
+import 'Module_14/class_1.dart';
 import 'module_11/GridV.dart';
 import 'module_11/M_11_Class_3.dart';
 import 'module_11/class_1.dart';
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
       // Use builder only if you need to use library outside ScreenUtilInit context
       builder: (_, child) {
         return MaterialApp(
-          initialRoute: '/BottomNav',
+          initialRoute: '/module14Class1',
           routes: {
             '/home': (context) => Home(),
             '/class1': (context) => Module_10_class_1(),
@@ -48,6 +49,7 @@ class MyApp extends StatelessWidget {
             '/Module13Class1': (context) => Module13Class1(),
             '/Module13class2': (context) => Module13class2(),
             '/BottomNav': (context) => BottomNav(),
+            '/module14Class1': (context) => module14Class1(),
 
           },
           debugShowCheckedModeBanner: false,
